065a4893fc0b7033cd4e0dd1499dd5dd450e32ec
Никитенко Илья <nikrob0494@gmail.com>
2024-09-11 18:09:02 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ from flask import Flask
 app = Flask (__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return '''<!doctype html> 
         <html>
            <body>
                <h1>web-сервер на flask</h1> 
+               <a href="/author">author</a>
            </body>
         </html> '''
+
+@app.route("/author")
+def author():
+    name = "Никитенко Илья Рманович"
+    group = "ФБИ-21"
+    faculty = "ФБ"
+
+    return '''<!doctype html>
+            <html>
+                <body>
+                    <p>Студент: ''' + name + '''</p>
+                    <p>Группа: ''' + group + '''</p>
+                    <p>Факультет ''' + faculty + '''</p>
+                    <a href="/web">web</a>
+                </body>
+            </html>'''
