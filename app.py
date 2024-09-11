4163b8703fca3c5984a46c0c4b5f09f9f0564dc2
Никитенко Илья <nikrob0494@gmail.com>
2024-09-11 16:49:04 +0700

    Вывод страницы первой лабаротоной

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask (__name__)
 
 @app.route("/")
-def strt():
-    return "web-сервер на flask"
+def start():
+    return "<!doctype html>" \
+        "<html>" \
+        "   <body>" \
+        "       <h1>web-сервер на flask</h1>" \
+        "   </body>" \
+        "</html>"
