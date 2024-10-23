62562f89a639e3a1b515798552992068362716c3
Никитенко Илья <nikrob0494@gmail.com>
2024-10-23 13:01:26 +0700

    Саммостоятельные задания Билет и Очистка куки

--- lab3.py
+++ lab3.py
@@ -87,3 +87,92 @@ def settings():
     
     resp = make_response(render_template('lab3/settings.html', color=color, fontsize=fontsize, backgroundcolor=backgroundcolor, fontfamily=fontfamily))
     return resp
+
+@lab3.route('/lab3/railway', methods=['GET', 'POST'])
+def railway():
+    errors = {}
+    user = ''
+    age = ''
+    sex = ''
+    place = ''
+    departure = ''
+    arrival = ''
+    date = ''
+    insurance = False
+
+    if request.method == 'POST':
+        user = request.form.get('user')
+        age = request.form.get('age')
+        sex = request.form.get('sex')
+        place = request.form.get('place')
+        departure = request.form.get('departure')
+        arrival = request.form.get('arrival')
+        date = request.form.get('date')
+        insurance = request.form.get('insurance')
+
+        # Проверка всех полей
+        if not user:
+            errors['user'] = 'Заполните поле!'
+        
+        if not age or not age.isdigit() or not (1 <= int(age) <= 120):
+            errors['age'] = 'Введите корректный возраст от 1 до 120 лет!'
+        
+        if not departure:
+            errors['departure'] = 'Заполните пункт отправления!'
+        
+        if not arrival:
+            errors['arrival'] = 'Заполните пункт прибытия!'
+        
+        if not date:
+            errors['date'] = 'Выберите дату!'
+
+        if not errors:
+            # Если ошибок нет, перенаправляем на страницу с оплатой
+            return redirect(url_for('lab3.payrail', user=user, age=age, sex=sex, place=place, 
+                                    departure=departure, arrival=arrival, date=date, 
+                                    insurance='on' if insurance else 'off'))
+
+    # Отображаем форму с ошибками (если есть) или просто GET запрос
+    return render_template(
+        'lab3/railway.html', 
+        user=user, age=age, sex=sex, place=place,
+        departure=departure, arrival=arrival, date=date, 
+        errors=errors
+    )
+
+
+@lab3.route('/lab3/payrail')
+def payrail():
+    user = request.args.get('user')
+    age = int(request.args.get('age'))
+    place = request.args.get('place')
+    insurance = request.args.get('insurance') == 'on'
+
+    # Расчет стоимости
+    price = 1000 if age >= 18 else 700
+
+    # Доплата за полку (нижняя или нижняя боковая)
+    if place in ['leftdawn', 'rightdawn']:
+        price += 100
+
+    # Доплата за страховку
+    if insurance:
+        price += 150
+
+    # Определяем тип билета
+    ticket_type = 'Взрослый билет' if age >= 18 else 'Детский билет'
+
+    return render_template('lab3/payrail.html', user=user, ticket_type=ticket_type, price=price)
+
+@lab3.route('/lab3/clear_settings')
+def clear_settings():
+
+    resp = make_response(redirect(url_for('lab3.settings')))
+    
+    resp.delete_cookie('color')
+    resp.delete_cookie('font-size')
+    resp.delete_cookie('background-color')
+    resp.delete_cookie('font-family')
+    
+    return resp
+
