fac5b25042804a7a94beba81f90f0cbd7b168f19
Никитенко Илья <nikrob0494@gmail.com>
2024-11-04 17:34:08 +0700

    Ссесия

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, make_response, redirect
+from flask import Blueprint, render_template, request, make_response, redirect, session
 lab4 = Blueprint('lab4', __name__)
 
 @lab4.route('/lab4/')
@@ -136,13 +136,27 @@ users = [
 @lab4.route('/lab4/login', methods = ['POST', 'GET'])
 def login():
     if request.method == 'GET':
-        return render_template('lab4/login.html', authorized=False)
+        if 'login' in session:
+            authorized = True
+            login = session['login']
+        else:
+            authorized = False
+            login =''
+        return render_template('lab4/login.html', authorized=authorized, login = login)
 
     login = request.form.get('login')
     password = request.form.get('password')
     
     for user in users:
         if login == user['login'] and password == user['password']:
-            return render_template('lab4/login.html', login=login, authorized = True)
+            session['login'] = login
+            return redirect('/lab4/login')
+            #return render_template('lab4/login.html', login=login, authorized = True)
     
     error = 'Неверное имя пользователя или пароль'
+
+
+@lab4.route('/lab4/logout', methods = ['POST'])
+def logout():
+    session.pop('login', None)
+    return redirect('/lab4/login')
