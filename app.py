fd15d521b21f70698432f422a651212c8cfa840c
Никитенко Илья <nikrob0494@gmail.com>
2024-12-16 21:46:43 +0700

    Модель базы данных

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, url_for, redirect, render_template, session
+import os
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -7,12 +12,27 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
-import os
+
 app = Flask (__name__)
 
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретныйключ')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'ilya_orm'
+    db_user = 'ilya_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+    app.config['SQLALCHEMY_DATABASE_URI']= f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "ilya_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
