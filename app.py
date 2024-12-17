383fa51ed174e1308cbb85e75da64a20d73d0ca6
Никитенко Илья <nikrob0494@gmail.com>
2024-12-17 19:28:12 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask (__name__)
 
@@ -51,6 +52,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 def zero():
@@ -103,6 +105,7 @@ def zero():
                         <li><a href="/lab6">Лабораторная 6</a></li>
                         <li><a href="/lab7">Лабораторная 7</a></li>
                         <li><a href="/lab8">Лабораторная 8</a></li>
+                        <li><a href="/lab9">Лабораторная 9</a></li>
                     <ul>
                 </main>
                 <footer>
