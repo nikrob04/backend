72207691c04b02f397343fee9ad7b500e62b980b
Никитенко Илья <nikrob0494@gmail.com>
2024-12-17 10:22:44 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,9 @@
 from flask import Blueprint, render_template, request, session, jsonify, redirect
+from db import db
+from db.models import users, articles
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user, login_required, current_user
+
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -7,17 +12,58 @@ lab8 = Blueprint('lab8', __name__)
 def lab8_index():
     return render_template('lab8/index.html')
 
-@lab8.route('/lab8/login/')
+@lab8.route('/lab8/login/', methods = ['GET', 'POST'])
 def login():
-    return render_template('lab8/login.html')
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
 
-@lab8.route('/lab8/register/')
+    user = users.query.filter_by(login =  login_form).first()
+
+    if not login_form or not password_form:
+        return render_template('lab8/login.html',
+                               error = 'Веведите логин/пароль')
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            return redirect ('/lab8/')
+    
+    return render_template ('lab8/login.html', error = 'Ошибка входа: неверный(ые) логин/пароль')
+    
+
+@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
 def register():
-    return render_template('lab8/register.html')
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+    
+    login_exists = users.query.filter_by(login =  login_form).first()
+    
+    if not login_form or not password_form:
+        return render_template('lab8/register.html',
+                               error = 'Веведите логин/пароль')
 
-@lab8.route('/lab8/articles')
+    if login_exists:
+        return render_template('lab8/register.html',
+                               error = 'Такой пользователь уже существует')
+    
+    
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/lab8/')
+    
+
+@lab8.route('/lab8/articles/')
+@login_required
 def articles():
-    return render_template('lab8/articles')
+    return 'Список статей'
 
 @lab8.route('/lab8/create')
 def create():
